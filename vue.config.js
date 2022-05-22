700b702aae0fac66e79dea7e4caf2d38a656fbfa
hyeminkim <orangetreex@gmail.com>
2022-05-22 23:20:03 +0900

    - backend 추가

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: { allowedHosts: 'all', },
+  devServer: {
+    allowedHosts: 'all',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  outputDir: './backend/public'
 })
